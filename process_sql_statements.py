2740a059a724d50a5ef3ad9d79170648ade1b6c0
Jeffrey Aven <jeffrey.aven@anz.com>
2019-09-16 15:45:58 +1000

    added support for multiple jobs and reading objects

--- process_sql_statements.py
+++ process_sql_statements.py
@@ -7,7 +7,7 @@
 # Example usage:
 # spark-submit process_sql_statements.py config.yml
 
-import yaml, sys
+import yaml, sys, datetime
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 
@@ -22,35 +22,49 @@ spark = SparkSession \
 	.builder \
 	.appName(config["name"]) \
 	.getOrCreate()
-spark.sparkContext.setLogLevel("WARN")
+spark.sparkContext.setLogLevel("ERROR")
 
-# Load Sources
-print("Creating source views...")
-for source in config["sources"]:
-	print("Creating view %s from table %s..." % (source["view"], source["table"]))
-	df = spark.table(source["table"])
-	if source.get("columns") is not None:
-		# columns listed, select given columns
-		df = df.select(source["columns"])
-	if source.get("filters") is not None:
-		for filter in source["filters"]:
-			df = df.filter(filter)
-	df.createOrReplaceTempView(source["view"])
+overall_start = datetime.datetime.now()
 
-# Perform Transforms
-print("Performing SQL Transformations...")
-for transform in config["transforms"]:
-	spark.sql(transform["sql"])
+for job in config["jobs"]:
+	# Load Sources
+	print("Creating source views...")
+	for source in job["sources"]:
+		if source.get("table") is not None:
+			print("Creating view %s from table %s..." % (source["view"], source["table"]))
+			df = spark.table(source["table"])
+		else:
+			print("Creating view %s from object %s..." % (source["view"], source["object"]))
+			df = spark.read.parquet(source["object"])
+		if source.get("columns") is not None:
+			# columns listed, select given columns
+			df = df.select(source["columns"])
+		if source.get("filters") is not None:
+			for filter in source["filters"]:
+				df = df.filter(filter)
+		df.createOrReplaceTempView(source["view"])
 
-# Write out final object
-print("Writing out final object to %s..." % (config["targets"]["target_location"]))
-final_df = spark.table(config["targets"]["final_object"])
-final_df.write.parquet(config["targets"]["target_location"], mode="overwrite")
-print("Finished writing out target object...")
+	# Perform Transforms
+	print("Performing SQL Transformations...")
+	for transform in job["transforms"]:
+		spark.sql(transform["sql"])
 
-### Remove this before productionizing 
-print("Sample output:")
-spark.read.parquet(config["targets"]["target_location"]).show()
-###
+	# Write out final object
+	print("Writing out final object to %s..." % (job["targets"]["target_location"]))
+	start = datetime.datetime.now()
+	final_df = spark.table(job["targets"]["final_object"])
+	final_df.write.parquet(job["targets"]["target_location"], mode="overwrite")
+	finish = datetime.datetime.now()
+	print("Finished writing out target object...")
 
-print("Done!")
+	### Remove this before productionizing
+	#print("Sample output:")
+	#spark.read.parquet(config["targets"]["target_location"]).show()
+	###
+
+	print("Total number of output rows: %s (%s)" % (str(spark.read.parquet(job["targets"]["target_location"]).count()), (str(finish-start))))
+
+overall_finish = datetime.datetime.now()
+
+print("total time taken : %s" % (str(overall_finish-overall_start)))
+spark.sparkContext.stop()
